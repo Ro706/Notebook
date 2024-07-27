ea7043210abbe3842508c229ffdd5843e5415b4b
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2024-07-27 08:02:53 +0530

    update file create  navbar homepage about page and Users page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar'; // Corrected path
+import About from './components/About';
+import Users from './components/Users';
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes instead of Switch
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes> {/* Changed from Switch to Routes */}
+          <Route path="/" element={<Home />} /> {/* Updated syntax */}
+          <Route path="/about" element={<About />} /> {/* Updated syntax */}
+          <Route path="/users" element={<Users />} /> {/* Updated syntax */}
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom"
+export default function Home() {
+  return (
+    <div>
+      <p>Home page</p>
+    </div>
+  )
+}
